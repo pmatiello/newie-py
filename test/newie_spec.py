fc2792a9c4006f241ffed93abcb516b1e86b8622
Pedro Matiello <pmatiello@gmail.com>
2010-12-07 20:21:22 -0200

    Test ported to Python.

---
+++ test/newie_spec.py
@@ -0,0 +1,4 @@
+class newie_spec():
+    
+    def should_instantiate_when_invoking_new(self):
+        assert object() != None
--- test/test_newie.rb
+++
@@ -1,7 +0,0 @@
-require 'helper'
-
-class TestNewie < Test::Unit::TestCase
-  should "instantiate when invoking new" do
-    assert_not_nil Object.new
-  end
-end
